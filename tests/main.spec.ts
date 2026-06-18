d035d397f6b2d35077e6802f0bffdb10ca169e2c
fairever <git@fairever.aleeas.com>
2026-06-18 09:23:14 +0800

    feat: ✨ save file to output dir

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,18 +1,26 @@
-import { appendFile } from 'fs/promises';
+import { appendFile, mkdir } from 'fs/promises';
 import { expect, Page, test } from 'patchright/test';
+import path from 'path';
 
-async function saveContent(page: Page) {
+const currentDir = import.meta.dirname;
+
+async function saveContent(page: Page, title: string) {
   await page.waitForLoadState('networkidle');
   const content = await page.locator('div.content-area').innerHTML();
-  await appendFile('output.html', content, 'utf-8');
+
+  let dir = path.join(currentDir, '../output', title);
+  let file = path.join(dir, 'output.html');
+
+  await mkdir(dir, { recursive: true })
+  await appendFile(file, content, 'utf-8');
 }
 
-async function saveNextPage(page: Page) {
+async function saveNextPage(page: Page, title: string) {
   const nextBtm = page.getByRole('link', { name: '下一页' });
   await expect(nextBtm).not.toHaveClass("disabled");
 
   await nextBtm.click();
-  await saveContent(page)
+  await saveContent(page, title)
 }
 
 async function waitForLogin(page: Page) {
@@ -23,16 +31,26 @@ async function openBook(page: Page, index: number) {
   await page.locator('div.book-cover').nth(index).locator('a').click();
 }
 
+async function getTitle(page: Page, index: number) {
+  let title = await page.locator('div.book-title').nth(index).getAttribute('title');
+  if (title != null) {
+    return title
+  }
+  process.exit(1)
+}
+
 test('main', async ({ page }) => {
   await page.goto('https://ritdon.com');
 
   await waitForLogin(page)
 
+  let title = await getTitle(page, 19)
+
   await openBook(page, 19)
 
-  await saveContent(page)
+  await saveContent(page, title)
 
   while (true) {
-    await saveNextPage(page)
+    await saveNextPage(page, title)
   }
 });
